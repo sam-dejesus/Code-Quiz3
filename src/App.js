73c9827abe077cbe7bf573446db43f69706ff481
samuel de jesus <samuel1de2jesus3@gmail.com>
2024-05-04 13:10:45 -0400

    updated styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Homepage from "./webpages/homepage/Homepage"; 
 
+
 function App() {
   return (
 
@@ -11,6 +12,7 @@ function App() {
   
       <Routes>
         <Route path="/" element={<Homepage />} />
+        
       </Routes>
  
 
--- src/webpages/homepage/Homepage.js
+++ src/webpages/homepage/Homepage.js
@@ -1,15 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./style.css"
+import "./style.css";
 
 const Home = () => {
+  const navigate = useNavigate();
+  function menu() {
+    navigate("Dashboard");
+  }
 
-    return(
-        <div className="d-flex justify-content-center align-items-center flex-column masterDiv">
-            <h1 className="headText">Welcome to Code Quiz App 3 😁</h1>
-            <p className="text">this is the third iteration of my code quiz app this version is more streamlined and will have much more advance features and game modes. I hope you enjoy</p>
-            <button>start</button>
-        </div>
-    )
-}
+  return (
+    <>
+      <div className="d-flex  align-items-center flex-column masterDiv">
+        <h1 className="headText">Code Quiz 3</h1>
+        <div className="Difficulty"></div>
+        {/* <p className="text">This is the third iteration of my code quiz app this version is more streamlined and will have much more advance features and game modes. I hope you enjoy</p> */}
+        <form className="gameMode border mt-5">
+          <input
+            type="button"
+            className="btn custom-btn"
+            value="Start"
+            onClick={menu}
+          />
+          <input
+            type="button"
+            className="btn custom-btn"
+            value="Start"
+            onClick={menu}
+          />
+          <input
+            type="button"
+            className="btn custom-btn"
+            value="Start"
+            onClick={menu}
+          />
+          <input
+            type="button"
+            className="btn custom-btn"
+            value="Start"
+            onClick={menu}
+          />
+        </form>
+      </div>
+    </>
+  );
+};
 export default Home;
